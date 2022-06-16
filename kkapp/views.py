456d840422cce181ec062804b652faf35d65480e
durga <durgasoftsolution.pvt.ltd@gmail.com>
2022-06-16 10:26:50 +0530

    first commit

---
+++ kkapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def abc(request):
+    return HttpResponse('<h1> WELCOME TO MY WEBSITE </h1>')
+
+# Create your views here.
